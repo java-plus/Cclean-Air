05109e27dc080f8198696e652eb1583abad337f5
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-10 11:28:00 +0200

    Creation projet spring

---
+++ src/main/java/dev/CcleanAirApplication.java
@@ -0,0 +1,13 @@
+package dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcleanAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcleanAirApplication.class, args);
+	}
+
+}
