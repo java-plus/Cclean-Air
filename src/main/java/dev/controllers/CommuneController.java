3ef297f40090448cb99268ca18f71f8184aac997
Cecile Peyras <peyrascecile@gmail.com>
2019-09-11 15:17:55 +0200

    visualiser données locales

--- src/main/java/dev/controllers/CommuneController.java
+++ src/main/java/dev/controllers/CommuneController.java
@@ -1,11 +1,14 @@
-package dev.controllers.dto;
+package dev.controllers;
 
+import dev.entities.DonneesLocales;
 import dev.exceptions.CommuneInvalideException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController (value = "/communes")
 public class CommuneController {
 
     @ExceptionHandler(CommuneInvalideException.class)
@@ -13,4 +16,6 @@ public class CommuneController {
         return ResponseEntity.status(404).body(e.getMessage());
     }
 
+    @GetMapping(value = "/{codeInsee}" )
+    public
 }
---
+++ src/main/java/dev/controllers/dto/DonneesLocalesDTO.java
@@ -0,0 +1,4 @@
+package dev.controllers.dto;
+
+public class DonneesLocalesDTO {
+}
