19ee062b237d05f3804f1295587402716d4291d5
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-12 16:28:37 +0200

    Ajout visualiser profil

--- src/main/java/dev/utils/ConditionMeteoSheduledTasks.java
+++ src/main/java/dev/utils/ConditionMeteoSheduledTasks.java
@@ -1,40 +1,41 @@
 package dev.utils;
 
-import dev.controllers.dto.CommuneDto;
-import dev.entities.ConditionMeteo;
-import dev.services.CommuneService;
-import dev.services.ConditionMeteoService;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import dev.controllers.dto.CommuneDto;
+import dev.entities.ConditionMeteo;
+import dev.services.CommuneService;
+import dev.services.ConditionMeteoService;
 
 @Component
 public class ConditionMeteoSheduledTasks {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(ConditionMeteoSheduledTasks.class);
+	private final Logger LOGGER = LoggerFactory.getLogger(ConditionMeteoSheduledTasks.class);
 
-    private ConditionMeteoService conditionMeteoService;
-    private CommuneService communeService;
+	private ConditionMeteoService conditionMeteoService;
+	private CommuneService communeService;
 
-    @Autowired
-    public ConditionMeteoSheduledTasks(ConditionMeteoService conditionMeteoService, CommuneService communeService) {
-        this.conditionMeteoService = conditionMeteoService;
-        this.communeService = communeService;
-    }
+	@Autowired
+	public ConditionMeteoSheduledTasks(ConditionMeteoService conditionMeteoService, CommuneService communeService) {
+		this.conditionMeteoService = conditionMeteoService;
+		this.communeService = communeService;
+	}
 
-    @Scheduled(cron="0 * * * * *")
-    public void recupererDonneesApiMeteoEtSauvegarder() {
-        LOGGER.info("lancement de recupererDonneesApiMeteoEtSauvegarder()");
-        List<CommuneDto> communes = communeService.recupererToutesLesCommunesDto();
-        for (CommuneDto c : communes) {
-            ConditionMeteo conditionMeteo = conditionMeteoService.recupererConditionMeteoCommune(c);
-            conditionMeteoService.sauvegarderConditionMeteo(conditionMeteo);
-        }
+	@Scheduled(cron = "0 * * * * *")
+	public void recupererDonneesApiMeteoEtSauvegarder() {
+		LOGGER.info("lancement de recupererDonneesApiMeteoEtSauvegarder()");
+		List<CommuneDto> communes = communeService.recupererToutesLesCommunesDto();
+		for (CommuneDto c : communes) {
+			ConditionMeteo conditionMeteo = conditionMeteoService.recupererConditionMeteoCommune(c);
+			conditionMeteoService.sauvegarderConditionMeteo(conditionMeteo);
+		}
 
-    }
+	}
 
 }
