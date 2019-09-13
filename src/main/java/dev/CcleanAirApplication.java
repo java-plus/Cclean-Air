b37f41bd048b2f7047eeca2b0695b7e913c740b4
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-13 16:25:34 +0200

    Ajout d'utilisateurs, communes et codes postaux

--- src/main/java/dev/CcleanAirApplication.java
+++ src/main/java/dev/CcleanAirApplication.java
@@ -1,24 +1,82 @@
 package dev;
 
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import dev.entities.CodePostal;
 import dev.entities.Commune;
+import dev.entities.Statut;
 import dev.entities.Utilisateur;
+import dev.repositories.ICodePostalRepository;
+import dev.repositories.ICommuneRepository;
+import dev.repositories.IUtilisateurRepository;
 
 @SpringBootApplication
 @EnableScheduling
 public class CcleanAirApplication {
 
+	@Autowired
+	IUtilisateurRepository utilisateurRepository;
+
+	@Autowired
+	ICommuneRepository communeRepository;
+
+	@Autowired
+	ICodePostalRepository codePostalRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void init() {
+
+		// création utilisateurs
+		List<Statut> listeStatuts = new ArrayList<>();
+		listeStatuts.add(Statut.ADMINISTRATEUR);
+		listeStatuts.add(Statut.MEMBRE);
+		var user1 = new Utilisateur("Cussonet", "Simon", "simon.cussonet@gmail.com", passwordEncoder.encode("1234"),
+				listeStatuts, Boolean.TRUE, Integer.valueOf(0), ZonedDateTime.now(), null, null);
+
+		List<Statut> listeStatuts2 = new ArrayList<>();
+		listeStatuts.add(Statut.MEMBRE);
+		var user2 = new Utilisateur("Nombidon", "Raoul", "raoul.nomBidon@gmail.com", passwordEncoder.encode("5678"),
+				listeStatuts2, Boolean.FALSE, Integer.valueOf(0), ZonedDateTime.now(), null, null);
+
+		// Création communes
+		var commune1 = new Commune("Quimper", Long.valueOf(50000), "29000", Double.valueOf(-1), Double.valueOf(2));
+		communeRepository.save(commune1);
+		List<CodePostal> CP = new ArrayList<>();
+		user1.setCommune(commune1);
+		var c1 = new CodePostal("29000", commune1);
+		codePostalRepository.save(c1);
+		CP.add(c1);
+		commune1.setCodesPostaux(CP);
+
+		var commune2 = new Commune("Lannion", Long.valueOf(25000), "22300", Double.valueOf(-2), Double.valueOf(3));
+		communeRepository.save(commune2);
+		user2.setCommune(commune2);
+		List<CodePostal> CP2 = new ArrayList<>();
+		var c2 = new CodePostal("22300", commune2);
+		codePostalRepository.save(c2);
+		CP2.add(c2);
+		commune2.setCodesPostaux(CP2);
+
+		utilisateurRepository.save(user1);
+		utilisateurRepository.save(user2);
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CcleanAirApplication.class, args);
-
-		Commune commune = new Commune();
-		commune.setNom("rennes");
-
-		Utilisateur u1 = new Utilisateur();
-		u1.setPrenom("jean-pierre");
 
 	}
 
