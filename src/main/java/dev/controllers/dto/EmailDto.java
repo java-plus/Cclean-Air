acc418c682742eda01000d57ca8f25cd4a34fafb
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-09-16 15:01:59 +0200

    Suppression de son propre compte effectuée

---
+++ src/main/java/dev/controllers/dto/EmailDto.java
@@ -0,0 +1,26 @@
+package dev.controllers.dto;
+
+public class EmailDto {
+	
+	private String email;
+	
+	public EmailDto() {
+		super();
+	}
+
+	public EmailDto(String email) {
+		super();
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
